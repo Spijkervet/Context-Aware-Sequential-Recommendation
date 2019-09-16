1f3b4216326f3cd3070ca9d258c1c5d94f89af43
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-09-16 16:25:49 +0200

    Changed run.job

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,2 @@
-cd data && curl http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books_5.json.gz -O
+cd data && wget -nc http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books_5.json.gz
 # cd data && curl http://snap.stanford.edu/data/amazon/productGraph/metadata.json.gz -O
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    limit = 5000
+    limit = None
+
+    if limit is None:
+        limit = 8899041
     reviews_books = DataReader('./data/reviews_Books_5.json.gz', limit=limit)
     reviews_books.preprocess('./data/preprocessed_books_data.csv')
