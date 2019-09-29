69df2701a8688d1103a5bc499d47856a1efd43bb
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-09-29 14:30:57 +0200

    Changed default to gpu

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 #SBATCH --job-name=ir2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
@@ -9,23 +8,27 @@
 #SBATCH --partition=gpu_shared_course
 #SBATCH --gres=gpu:2
 
-
 module purge
 module load eb
 
+# module load cuDNN/7.0.5-CUDA-9.0.176
+# module load cuDNN/7.4.2-CUDA-10.0.130
+# module load NCCL/2.0.5-CUDA-9.0.176
+# export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.4.2-CUDA-10.0.130/lib64:$LD_LIBRARY_PATH
+
+# For TensorFlow 2.0, we require CUDA >=10.0 and CuDNN >=7.4
 module load Python/3.6.3-foss-2017b
-module load cuDNN/7.0.5-CUDA-9.0.176
-module load NCCL/2.0.5-CUDA-9.0.176
-export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+module load CUDA/10.0.130
+export LD_LIBRARY_PATH=$HOME/sdk/cuda/lib64:$LD_LIBRARY_PATH
 
 # Mail
-echo "[IR2] Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
+mail_template "IR2" $SLURM_JOBID "STARTED" "$1"
 
 pip3 install virtualenv 
 python3 -m virtualenv ir2
 
 . ir2/bin/activate
-
 pip3 install -r requirements.txt
 
 # DOWNLOAD DATA
@@ -38,7 +41,7 @@ sh download_data.sh
 # MOVIELENS 1-M
 python3 preprocess.py --raw_dataset data/ml-1m/ratings.dat --type movielens --dataset data/ml-1m.txt
 
-
 ### PROGRAM ###
-python3 main.py --dataset data/ml-1m.txt --train_dir maxlen_200_dropout_0.2 --maxlen=200 --dropout_rate=0.2
-echo "[IR2] Job $SLURM_JOBID finished at `date`" | mail $USER -s "Job $SLURM_JOBID"
+python3 main.py --dataset data/ml-1m.txt --train_dir tf2_epoch_500_maxlen_200_dropout_0.2 --maxlen=200 --dropout_rate=0.2 --num_epochs 500
+
+mail_template "IR2" $SLURM_JOBID "FINISHED" "$1"
