d1557e542c3f1047c38fd43f3a57579194480b6d
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-10-17 01:56:37 +0200

    Verified time_lstm baseline

---
+++ baselines/time_lstm_orig/log.py
@@ -0,0 +1,13 @@
+import os
+
+def log_results(result_dir, current_epoch, recall10, MRR10_score, avg_rate, loss, name):
+
+    p = os.path.join(result_dir, 'results.txt')
+    if not os.path.exists(p):
+        with open(p, 'w') as f:
+            f.write('epoch,recall10,mrr10,avg_rate,loss,name\n')
+
+    with open(p, 'a+') as f:
+        f.write('{},{},{},{},{},{}\n'.format(current_epoch, recall10, MRR10_score, avg_rate, loss, name))
+    
+    print(current_epoch, recall10, MRR10_score, avg_rate, loss, name)
---
+++ baselines/time_lstm_orig/plot.py
@@ -0,0 +1,42 @@
+import os
+import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+if __name__ == '__main__':
+    # args = argparse.ArgumentParser()
+    # # args.add_argument('--file', required=True, type=str, help='The results file located in ./log')
+    # args = args.parse_args()
+
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+
+    rootDir = 'log'
+    for d, _, files in os.walk(rootDir):
+        for f in files:
+            if f == 'results.txt':
+                f = os.path.join(d, f)
+                with open(f, 'r'):
+                    df = pd.read_csv(f)
+
+                    out_dir = os.path.join('plots', os.path.dirname(f).split('/')[1])
+                    if not os.path.exists(out_dir):
+                        os.makedirs(out_dir)
+
+                    plt.figure()
+                    plt.plot(df['epoch'], df['recall10'])
+                    plt.title('recalll10')
+                    plt.xlabel('epoch')
+                    plt.ylabel('recall10')
+                    plt.savefig(os.path.join(out_dir, 'recall10.png'))
+
+                    plt.figure()
+                    plt.plot(df['epoch'], df['mrr10'])
+                    plt.title('mrr10')
+                    plt.xlabel('epoch')
+                    plt.ylabel('mrr10')
+                    plt.savefig(os.path.join(out_dir, 'mrr10.png'))
