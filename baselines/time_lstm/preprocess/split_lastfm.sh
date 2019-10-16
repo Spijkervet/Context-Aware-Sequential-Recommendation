c65795797b7f3e29fb5fcbe58ec03f71e07dae5a
Anton S <antons@live.nl>
2019-10-16 19:55:22 +0200

    split according to authors

---
+++ baselines/time_lstm/preprocess/split_lastfm.sh
@@ -0,0 +1,6 @@
+tail -192 user-item.lst > te_user-item.lst
+tail -192 user-item-delta-time.lst > te_user-item-delta-time.lst
+tail -192 user-item-accumulate-time.lst > te_user-item-accumulate-time.lst
+head -800 user-item.lst > tr_user-item.lst
+head -800 user-item-delta-time.lst > tr_user-item-delta-time.lst
+head -800 user-item-accumulate-time.lst > tr_user-item-accumulate-time.lst
