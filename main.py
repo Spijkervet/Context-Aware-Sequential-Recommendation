7219534468d88323b9394870ea4f6ee1cea9b40b
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-10-20 13:51:50 +0200

    Merging main

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
             model = CAST5(usernum, itemnum, ratingnum, args)
     elif args.model == "cast_6":
         model = CAST6(usernum, itemnum, ratingnum, args)
-    elif args.model == "sasrec" or args.test_baseline:
+    elif args.model == "sasrec": 
         model = SASRec(usernum, itemnum, args)
     elif args.model == "sasrec_static":
         model = SASRec(usernum, itemnum, args, static=True)
